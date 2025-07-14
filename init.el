095f699f82e863d95c152dfb991b21cf5e2daa4e
oncomouse <oncomouse@gmail.com>
2025-07-14 08:47:46 -0500

    fix: required function

--- init.el
+++ init.el
@@ -195,6 +195,18 @@
 
   ;; Set the default coding system for files to UTF-8.
   (modify-coding-system-alist 'file "" 'utf-8)
+
+  ;; Display some modes in full-frame
+  (defun sanityinc/display-buffer-full-frame (buffer alist)
+    "If it's not visible, display buffer full-frame, saving the prior window config.
+The saved config will be restored when the window is quit later.
+BUFFER and ALIST are as for `display-buffer-full-frame'."
+    (let ((initial-window-configuration (current-window-configuration)))
+      (or (display-buffer-reuse-window buffer alist)
+          (let ((full-window (display-buffer-full-frame buffer alist)))
+            (prog1
+                full-window
+              (set-window-parameter full-window 'sanityinc/previous-config initial-window-configuration))))))
   (defmacro sanityinc/fullframe-mode (mode)
     "Configure buffers that open in MODE to display in full-frame."
     `(add-to-list 'display-buffer-alist
